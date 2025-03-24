465acea0a5ba5c9cb1682c18b3421fad6da63a7f
Vitalij <krinickij.v.a@gmail.com>
2025-03-24 23:07:36 +0300

    FEAT: added a couple of words of thanks + styles for 404Error page
    Добавлена пара благодарственных слов + конатктные данные на главную старницу
    Добавли немного стилей на страницу 404

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,4 +1,22 @@
 <template>
-404
+<MainLayout>
+    <div class="error">
+        Упс 404...
+    </div>
+
+</MainLayout>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainLayout from '../components/layout/MainLayout.vue';
+
+</script>
+<style lang="scss" scoped>
+    .error{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-top: 100px;
+        font-weight: 700;
+        font-size: 50px;
+    }
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,22 @@
 <template>
     <MainLayout>
+        <div class="main-page">
         <h1>Главная разводящая страница</h1>
-        
+        Спасибо что потратили своё время на ознакомление с моей работой!
+        <a href="mailto:krinickij.v.a@gmail.com">krinickij.v.a@gmail.com</a>
+        <a href="https://t.me/krinickijva">tg: @krinickijva</a>
+    </div>
     </MainLayout>
 </template>
 <script setup lang="ts">
 import MainLayout from '../components/layout/MainLayout.vue'
 </script>
+<style lang="scss" scoped>
+    .main-page{
+        width: 600px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        margin: 25px auto;
+    }
+</style>
