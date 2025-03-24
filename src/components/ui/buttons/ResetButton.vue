b3c4efcb993e36a5341d3259d40d6b65eb77c054
Vitalij <krinickij.v.a@gmail.com>
2025-03-24 21:12:28 +0300

    FEAT: aded button components
    Добавлены переиспользуемые компоненты кнопок  подтвердить и сбросить

---
+++ src/components/ui/buttons/ResetButton.vue
@@ -0,0 +1,25 @@
+<template>
+<button type="reset">{{ title }}</button>
+</template>
+<script setup lang="ts">
+interface Props{
+    title:string
+}
+defineProps<Props>()
+</script>
+<style scoped lang="scss">
+    button {
+        padding: 10px;
+        border-radius: 20px;
+        background-color: red;
+        color: #000000;
+        transition: all .3s linear;
+        font-weight: 500;
+        font-size: 20px;
+    &:hover{
+        color: #FFFFFF;
+        background-color: rgb(161, 9, 9);
+        transition: all .3s linear;
+    }
+    }
+</style>
---
+++ src/components/ui/buttons/SubmitButton.vue
@@ -0,0 +1,26 @@
+<template>
+<button type="submit">{{ title }}</button>
+</template>
+<script lang="ts" setup>
+interface Props {
+    title:string
+}
+defineProps<Props>()
+
+</script>
+<style lang="scss" scoped>
+button{
+    padding: 10px;
+    border-radius: 20px;
+    background-color: gainsboro;
+    color: #000000;
+    transition: all .3s linear;
+    font-weight: 500;
+    font-size: 20px;
+    &:hover{
+        color: #FFFFFF;
+        background-color: gray;
+        transition: all .3s linear;
+    }
+}
+</style>
